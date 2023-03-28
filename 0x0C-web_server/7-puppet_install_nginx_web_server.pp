e8508e673c69eb2f0d3eb1fda2bb2a12eb69392f
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-03-28 14:07:24 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,23 @@
-0x0C-web_server
+# Install nginx with puppet
+package { 'nginx':
+  ensure   => '1.18.0',
+  provider => 'apt',
+}
+
+file { 'Hello World':
+  path    => '/var/www/html/index.nginx-debian.html',
+  content => 'Hello World',
+}
+
+file_line { 'Hello World':
+  path  => '/etc/nginx/sites-available/default',
+  after => 'server_name _;',
+  line  => '\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+exec { 'service':
+  command  => 'service nginx start',
+  provider => 'shell',
+  user     => 'root',
+  path     => '/usr/sbin/service',
+}
