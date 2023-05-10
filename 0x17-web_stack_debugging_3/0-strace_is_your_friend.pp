996c70e0fc2645d2ad7438858e5d299214bc291c
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-05-10 01:40:54 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,6 @@
-0x17-web_stack_debugging_3
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
