2c8d1ae616c0c59ef1d1b2a821648b9aced29c3b
Achilihu Daniel Uchenna <107114779+Achilihudaniel07@users.noreply.github.com>
2023-05-21 12:31:28 +0100

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,1 +1,13 @@
-0x1B-web_stack_debugging_4
+
+f high amount of requests
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
